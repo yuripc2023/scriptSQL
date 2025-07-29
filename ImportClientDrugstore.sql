bfaf89a65c005b9858770b108f4a55c063b3ce30
Yuri <yuripc@atic.pe>
2025-07-29 10:33:33 -0500

    Add script for import Clients of Drugstore

---
+++ ImportClientDrugstore.sql
@@ -0,0 +1,6 @@
+use dbdisucII
+select * from maeaso
+
+select asocod as CodeI, ASOROL, ASONRZ as FirstName, ASOTDO as TypeDocument, ASONDO as NumberDocument, ASONCO as OtherName, ZONCOD as Zona from maeaso 
+where asorol = 'Cliente'
+
